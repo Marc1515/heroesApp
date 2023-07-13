538a51cba97d97ce0ecc4b9f858e6de9f886d941
Marc España Quinquilla <meq.1515@gmail.com>
2023-07-13 11:29:17 +0200

    Comprobamos que obtenemos los datos de los héroes y los plasmamos con simples <li> en el Frontend.

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesServices } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-list-page',
@@ -6,6 +8,16 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+
+  public heroes: Hero[] = [];
+
+  constructor(private heroesServices: HeroesServices) {}
+
+
+  ngOnInit(): void {
+    this.heroesServices.getHeroes()
+      .subscribe( heroes => this.heroes = heroes );
+  }
 
 }
