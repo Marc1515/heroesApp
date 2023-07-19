02935d2b53de699ab4fa7722c190cbf76d59eb63
Marc España Quinquilla <meq.1515@gmail.com>
2023-07-19 16:49:01 +0200

    Dejamos medio configurado el checkAuthentication() en el auth.service.ts para que compruebe no solo que le llega un User, sino que también el token para verificar.

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { enviroments } from 'src/enviroments/enviroments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -19,11 +19,25 @@ export class AuthService {
 
   login( email: string, password: string ): Observable<User> {
 
-      return this.http.get<User>(`${ this.baseUrl }/users/1`)
-        .pipe(
-          tap( user => this.user = user),
-          tap( user => localStorage.setItem('token', 'oidgkdr.fsefef.fwefwef') )
-        )
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user),
+        tap( user => localStorage.setItem('token', 'oidgkdr.fsefef.fwefwef') )
+      )
+  }
+
+  checkAuthentication(): Observable<boolean> | boolean {
+
+    if ( !localStorage.getItem('token')) return false;
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        map( user => !!user ),
+        catchError( err => of(false) )
+      )
 
   }
 
